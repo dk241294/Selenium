bb29d4bbb1e9aa931b955f20a11f2aa8d5e37a17
Deepak Kumar <deepak24121994@gmail.com>
2020-05-31 21:32:50 +0530

    xpath and css

--- Demo/src/main/java/locaters/Facebook.java
+++ Demo/src/main/java/locaters/Facebook.java
@@ -6,16 +6,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Facebook {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://facebook.com");
         //id  locator
-        driver.findElement(By.id("email")).sendKeys("deepak");
+        // driver.findElement(By.id("email")).sendKeys("deepak");
         //name Locator
-        driver.findElement(By.name("pass")).sendKeys("12345");
+        // driver.findElement(By.name("pass")).sendKeys("12345");
+        //linktext Locator
+        //driver.findElement(By.linkText("Forgotten account?")).click();
+        //xpath
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("dinesh");
+        //by css
+        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("password");
         //linktext Locator
         driver.findElement(By.linkText("Forgotten account?")).click();
+        Thread.sleep(3000);
 
         driver.close();
 
--- Demo/src/main/java/locaters/SalesForce.java
+++ Demo/src/main/java/locaters/SalesForce.java
@@ -8,17 +8,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SalesForce {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.salesforce.com/in");
         driver.findElement(By.linkText("Login")).click();
+
         Thread.sleep(2000);
         driver.findElement(By.id("username")).sendKeys("rajkapoor");
         driver.findElement(By.name("pw")).sendKeys("789456");
         //compound class name not accepted throw error
-       // driver.findElement(By.className("button r4 wide primary")).click();
+        // driver.findElement(By.className("button r4 wide primary")).click();
         driver.findElement(By.id("Login")).click();
+        System.out.println(driver.findElement(By.id("error")).getText());
+        // System.out.println(driver.findElement(By.cssSelector("div[class='loginError']")).getText());
 
         Thread.sleep(2000);
+
         driver.close();
     }
 }
