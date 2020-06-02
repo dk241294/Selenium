695a968040706476084fcaa964e25b44ff8c2426
Deepak Kumar <deepak24121994@gmail.com>
2020-06-02 13:42:30 +0530

    parent child relationship

--- Demo/src/main/java/locaters/Google.java
+++ Demo/src/main/java/locaters/Google.java
@@ -2,6 +2,7 @@ package locaters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,9 +11,13 @@ public class Google {
         WebDriverManager.chromedriver().setup();
         WebDriver driver =new ChromeDriver();
         driver.get("https://google.com");
-        driver.findElement(By.xpath("//div[@class='RNNXgb']/div/div[2]/input")).sendKeys("Today news");
-       // driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']/center/input")).click();
-        driver.findElement(By.xpath("//div[@id='gb']/div/div/div/div/div[2]/a")).click();
+        driver.findElement(By.xpath("//div[@class='RNNXgb']/div/div[2]/input")).sendKeys("sharukh");
+        Thread.sleep(2000);
+        //driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']/center/input")).click();
+        driver.findElement(By.xpath("//div[@class='RNNXgb']/div/div[2]/input")).sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+        //driver.findElement(By.xpath("//div[@id='gb']/div/div/div/div/div[2]/a")).click();
+        driver.findElement(By.xpath("//div[@id='hdtb-msb']/div/div/div[2]/a")).click();
         Thread.sleep(2000);
         driver.quit();
     }
