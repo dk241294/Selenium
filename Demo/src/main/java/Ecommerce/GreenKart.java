53d848eb63e469c9e67cf670f55df62946078a58
Deepak Kumar <deepak24121994@gmail.com>
2020-06-09 11:31:47 +0530

    selecting the desired  item

--- Demo/src/main/java/Ecommerce/GreenKart.java
+++ Demo/src/main/java/Ecommerce/GreenKart.java
@@ -9,18 +9,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 public class GreenKart {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         kart(driver);
+        Thread.sleep(4000);
+        driver.quit();
 
     }
 
-    public static void kart(WebDriver driver) {
+    public static void kart(WebDriver driver) throws InterruptedException {
         driver.get("https://rahulshettyacademy.com/seleniumPractise");
+        Thread.sleep(2000);
         List<WebElement> product = driver.findElements(By.cssSelector("h4.product-name"));
-        for(int i=0;i<product.size();i++){
-            String productName=product.get(i).getText();
+        for (int i = 0; i < product.size(); i++) {
+            String productName = product.get(i).getText();
+            if (productName.contains("Cucumber")) {
+                System.out.println(productName);
+                //find elements because all have same xpath then get(i) to differentiate and click the right one
+                driver.findElements(By.xpath("//*[text()='ADD TO CART']")).get(i).click();
+                break;
+            }
         }
 
     }
