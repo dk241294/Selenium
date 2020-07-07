9fa43b96fbe41f5da0b2464069d384fe7736e502
Deepak Kumar <deepak24121994@gmail.com>
2020-07-07 21:19:46 +0530

    array

---
+++ Demo/src/main/java/miscellaneous/Array/PracticeArray.java
@@ -0,0 +1,18 @@
+package miscellaneous.Array;
+
+public class PracticeArray {
+
+    public static void main(String[] args) {
+        int a[] = new int[3];
+        a[0] = 10;
+        a[1] = 2;
+        a[2] = 25;
+        for (int i=0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        int b[]={2,32,566,66};
+        for (int i=0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+    }
+}
