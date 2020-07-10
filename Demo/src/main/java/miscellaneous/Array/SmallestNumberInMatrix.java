e41aad567d0ab0ddfaaf83fdb15f0875be4ce9d9
Deepak Kumar <deepak24121994@gmail.com>
2020-07-10 11:14:36 +0530

    largest in column of smallest in matrix

--- Demo/src/main/java/miscellaneous/Array/SmallestNumberInMatrix.java
+++ Demo/src/main/java/miscellaneous/Array/SmallestNumberInMatrix.java
@@ -7,17 +7,41 @@ import com.sun.jdi.PathSearchingVirtualMachine;
 
 public class SmallestNumberInMatrix {
     public static void main(String[] args) {
-        int a[][] = {{2, 4, 6}, {44, 68, 3}, {7, 8, 66}};
+        SmallestNumberInMatrix sb = new SmallestNumberInMatrix();
+        sb.max();
+        sb.maxInCol();
+
+
+    }
+
+    public void max() {
+        int a[][] = {{222, 4, 6}, {44, 68, 3}, {7, 8, 66}};
         int smallest = a[0][0];
+        int biggest = a[0][0];
+        int col = 0;
+        int j;
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+            for (j = 0; j < 3; j++) {
                 if (smallest > a[i][j]) {
                     smallest = a[i][j];
+                    col = j;
 
                 }
-
             }
+            if (smallest < a[i][col]) {
+                biggest = a[i][col];
+            }
+
+
         }
+
+
         System.out.println(smallest);
+        System.out.println(biggest);
+
+    }
+
+    public void maxInCol() {
+
     }
 }
