74ac5c35ed1a8cffeb86ba9a9d1ed2d10f338c08
Deepak Kumar <deepak24121994@gmail.com>
2020-07-21 19:46:37 +0530

    Arraylist

---
+++ Demo/src/main/java/Collection/ArrayListExample.java
@@ -0,0 +1,20 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(2);
+        a.add(31);
+        a.add(21);
+        a.add(34);
+        a.add(25);
+        a.add(35);
+        System.out.println( a.get(1));;
+        a.remove(2);
+        a.remove(new Integer(21));
+        System.out.println(a);
+        System.out.println( a.indexOf(new Integer(31))); ;
+    }
+}
