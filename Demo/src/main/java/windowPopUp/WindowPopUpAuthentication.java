c1864de96919a5b5504856d691baf56407141300
Deepak Kumar <deepak24121994@gmail.com>
2020-07-23 22:02:03 +0530

    window pop up handling

---
+++ Demo/src/main/java/windowPopUp/WindowPopUpAuthentication.java
@@ -0,0 +1,17 @@
+package windowPopUp;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowPopUpAuthentication {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://admin:admin@the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Basic Auth")).click();
+        System.out.println(driver.findElement(By.id("content")).getText());;
+
+    }
+}
