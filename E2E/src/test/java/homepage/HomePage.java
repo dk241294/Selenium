ef0f8171f14fac3b19d30208928fdf870b1acced
Deepak Kumar <deepak24121994@gmail.com>
2020-09-02 11:09:41 +0530

    update

--- E2E/src/test/java/homepage/HomePage.java
+++ E2E/src/test/java/homepage/HomePage.java
@@ -9,17 +9,22 @@ import pageObject.LandingPage;
 import pageObject.LoginPage;
 
 import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class HomePage extends Base {
+    public static Logger log = LogManager.getLogManager().getLogger(Base.class.getName());
+
     @BeforeTest
     public void initialize() throws IOException {
         driver = initializeDriver(); //it return driver and we have driver object in base class which we inherited
-
+        log.info("Driver is initialized");
     }
 
     @Test(dataProvider = "getData")
     public void basePageNavigation(String name, String password) throws IOException, InterruptedException {
         driver.get(prop.getProperty("url"));
+        log.info("navigated to home page");
         //driver.getTitle();
         // driver.findElement(B)
         LandingPage lp = new LandingPage(driver);
@@ -29,11 +34,13 @@ public class HomePage extends Base {
         loginPage.getEmail().sendKeys(name);
         loginPage.getPassword().sendKeys(password);
         loginPage.getLoginButton().click();
+        log.info("successfully validate");
     }
 
     @AfterTest
     public void tearDown() {
         driver.close();
+        log.info("");
     }
 
     @DataProvider
